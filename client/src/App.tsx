517ae8e8709204530b179dceba38172ff468e5fd
Frank <temitopeadeoye787898@gmail.com>
2025-08-29 21:31:54 +0100

    used the router features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
 //import { useState } from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   const isOwnerPage: boolean = useLocation().pathname.includes('owner');
 
   return (
-    <div className='min-h-[200vh]'>
+    <div>
       {!isOwnerPage && <Navbar/>}
+
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
